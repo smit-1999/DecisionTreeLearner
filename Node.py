e7853486db9d33fb83486727fd541cb25de696dc
Smit Shah <smitshah1999@gmail.com>
2023-09-29 01:45:46 -0500

    add parent field in node class

--- Node.py
+++ Node.py
@@ -1,9 +1,10 @@
 class Node():
-    def __init__(self, index=None, threshold=None, leftChild=None, rightChild=None, info_gain=None, value=None):
+    def __init__(self, index=None, threshold=None, leftChild=None, rightChild=None, info_gain=None, value=None, parent=None):
         self.index = index
         self.threshold = threshold
         self.leftChild = leftChild
         self.rightChild = rightChild
         self.info_gain = info_gain
+        self.parent = parent
 
         self.value = value
